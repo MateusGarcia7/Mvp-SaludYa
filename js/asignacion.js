117f0f24143b048fc91f677b80897ed25a3af150
Y3CM <pauljons27@gmail.com>
2026-06-03 23:04:02 -0500

    feat :crear citas

--- js/asignacion.js
+++ js/asignacion.js
@@ -1,4 +1,7 @@
 const usuarioLogueado = JSON.parse(localStorage.getItem("usuarioLogueado"));
+const registerForm = document.getElementById("register-form");
+const usuario = JSON.parse(localStorage.getItem("usuarios")); 
+
 
 if (!usuarioLogueado) {
   window.location.href = "login-paciente.html";
@@ -10,3 +13,34 @@ logout.addEventListener("click", e => { localStorage.removeItem("usuarioLogueado
 let bienvenida = document.getElementById("bienvenida");
 
 bienvenida.textContent = `Bienvenido, ${usuarioLogueado.nombre}`;
+
+registerForm.addEventListener("submit", e => {
+  e.preventDefault()
+const motivoConsulta = document.getElementById("motivo-consulta");
+const fechaDeseada = document.getElementById("fecha-deseada");
+const tipoAtencion = document.getElementById("tipo-atencion");
+const doctorSelect = document.getElementById("doctor-select");
+
+let citas = JSON.parse(localStorage.getItem("citas")) || [];
+
+const nuevaCita = {
+  id: Date.now(),
+  paciente: usuarioLogueado.nombre,
+  cc: usuario.documentNumber,
+  motivoConsulta: motivoConsulta.value,
+  fechaDeseada: fechaDeseada.value,
+  tipoAtencion: tipoAtencion.value,
+  doctor: doctorSelect.value,
+};
+citas.push(nuevaCita);
+localStorage.setItem("citas", JSON.stringify(citas));
+alert("Cita registrada exitosamente");
+registerForm.reset();
+
+});
+
+
+paciente.textContent = `Paciente: ${usuarioLogueado.nombre}`
+
+
+
